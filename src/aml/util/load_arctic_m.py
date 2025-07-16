a7134d4011461612a23565b7b6cd790fee6b63d0
Tryner <spamisttoll@web.de>
2025-07-17 01:04:44 +0200

    fix error with path

--- src/aml/util/load_arctic_m.py
+++ src/aml/util/load_arctic_m.py
@@ -13,5 +13,5 @@ def register_gte():
 def load_arctic_m(model_path: str | None = None):
     register_gte()
     if model_path is None:
-        model_path = files(models).joinpath("snowflake-arctic-embed-m-v2.0")
+        model_path = str(files(models).joinpath("snowflake-arctic-embed-m-v2.0"))
     return SentenceTransformer(model_path, config_kwargs={"use_memory_efficient_attention": False})
